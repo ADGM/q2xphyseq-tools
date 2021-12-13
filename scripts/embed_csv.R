3dbe3f7a700861ea8d715a7a68236dfaec33aef5
ieo4805 <adgmacandog@gmail.com>
2021-12-13 10:20:01 +0100

    211213_commit

---
+++ scripts/embed_csv.R
@@ -0,0 +1,14 @@
+embed_data= function(x= mtcars, filename= "file.csv", label= "Download as CSV"){
+  
+  # Create encoded Base64 datastream 
+  encode_data= function(x){
+    write.csv2(x, "./file.csv")
+    enc= sprintf('data:text/csv;base64,%s', openssl::base64_encode(paste0(readLines("./file.csv"), collapse="\n")) )
+    unlink("./file.csv")
+    return(enc)
+  }
+  
+  # String result ready to be placed in rmarkdown
+  paste0("<a download='", filename, "' href=", encode_data(x), ">", label, "</a>")
+  
+}
