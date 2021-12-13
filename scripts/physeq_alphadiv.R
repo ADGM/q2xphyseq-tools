8e0f00fd106ee4eb641a98222c31ce2e01a1b6e7
ieo4805 <adgmacandog@gmail.com>
2021-12-13 10:27:00 +0100

    211213_fix-print-sd

--- scripts/physeq_alphadiv.R
+++ scripts/physeq_alphadiv.R
@@ -3,8 +3,8 @@ physeq_alphadiv=function(physeq,group) {
 ps.rarefied = rarefy_even_depth(physeq, rngseed=1, sample.size=min(sample_sums(physeq)), replace=F)
 #factor variables before declaring new variable for collating alphadiv vals
 
-sd=min(sample_sums(physeq)
-print(paste0("Rarefied at ",sd," depth"))
+print_sd=paste0("Rarefied at ",min(sample_sums(physeq)," depth")
+print(print_sd)
 
 alpha.vals=estimate_richness(ps.rarefied, measures=c("Observed","Chao1","Shannon","Fisher","Simpson","InvSimpson"))
 rownames(alpha.vals)=gsub("X","",rownames(alpha.vals))
