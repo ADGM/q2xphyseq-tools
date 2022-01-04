d803dacb9dc41407c803fc8f30f7ade3540a6287
ieo4805 <adgmacandog@gmail.com>
2022-01-04 16:38:21 +0100

    220104_take-out-jitter-var

--- scripts/physeq_alphadivplot.R
+++ scripts/physeq_alphadivplot.R
@@ -1,11 +1,9 @@
-physeq_alphadivplot=function(physeq,group,jitter) {
+physeq_alphadivplot=function(physeq,group) {
 
 group=sym(group)
-jitter=sym(jitter)
-
 value=sym("value")
 
 p=ggplot(alpha.vals,aes(x=!!group,y=!!value,color=!!group)) + facet_wrap(. ~ alphadiv_metric, scales="free_y") + geom_boxplot(width=0.3,alpha=0.5,outlier.alpha=0) +
-geom_jitter(size=1,alpha=.8,aes(color=!!jitter)) + scale_x_discrete(guide = guide_axis(angle = 90)) + ggtitle(paste0("Alpha-diversity, by ",group)) + scale_color_viridis_d(option="H",begin=0.1,end=0.9) 
+geom_jitter(size=1,alpha=.8,aes(color=!!group)) + scale_x_discrete(guide = guide_axis(angle = 90)) + ggtitle(paste0("Alpha-diversity, by ",group)) + scale_color_viridis_d(option="H",begin=0.1,end=0.9) 
 
 }
