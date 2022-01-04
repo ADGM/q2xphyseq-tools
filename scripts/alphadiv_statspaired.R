35165ac79776ad6af3dc0e009c1732e9c23481c8
ieo4805 <adgmacandog@gmail.com>
2022-01-04 18:00:54 +0100

    2201014_first-commit

---
+++ scripts/alphadiv_statspaired.R
@@ -0,0 +1,11 @@
+alphadiv_stats=function(df,group) {
+
+f=as.formula(paste0("value ~ ",group))
+
+stats=df %>%
+  group_by(alphadiv_metric) %>%
+  rstatix::wilcox_test(f,paired=TRUE) %>%
+  rstatix::adjust_pvalue(method = "fdr") %>%
+  rstatix::add_xy_position(x=group)
+
+}
