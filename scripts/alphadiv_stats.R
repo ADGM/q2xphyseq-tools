d2e7c019bb1d02d032a7669e32b30f36ae162a34
ieo4805 <adgmacandog@gmail.com>
2022-01-04 17:06:14 +0100

    220104_add-plot-var

--- scripts/alphadiv_stats.R
+++ scripts/alphadiv_stats.R
@@ -1,15 +1,15 @@
-alphadiv_stats=function(alpha.vals,group) {
+alphadiv_stats=function(df,plot,group) {
 
 f=as.formula(paste0("value ~ ",group))
 
-stats=alpha.vals %>%
+stats=df %>%
   group_by(alphadiv_metric) %>%
   rstatix::wilcox_test(f) %>%
   rstatix::adjust_pvalue(method = "fdr") %>%
   rstatix::add_xy_position(x=group)
 
   # Add 10% spaces between the p-value labels and the plot border
-p + stat_pvalue_manual(stats,size = 2.5,label = paste0("        p = ","{p.adj}"),remove.bracket = TRUE,y.position=0.5)
+plot + stat_pvalue_manual(stats,size = 2.5,label = paste0("        p = ","{p.adj}"),remove.bracket = TRUE,y.position=0.5)
 
 datatable(stats,caption="Wilcoxon test by ",group)
 }
